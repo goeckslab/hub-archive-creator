2030e8475d6b79a1b67439dbf4b6ad7d95e371e9
Remi Marenco <remi.marenco@gmail.com>
2016-01-22 09:57:56 -0500

    Add the skeleton of the Galaxy XML + the tool with gff3 => genePred handling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JsCoverageQC
 This Galaxy tool permits to prepare your files to be ready for Assembly Hub visualization.
+
+To try the software, you need to have a .gff3 as input and set an output filename:
+```hubArchiveCreator -i myGff3.gff3 -o myOutput.zip```
+
+For now, the progran is not outputting a zip but a genepred because WIP
---
+++ hubArchiveCreator.py
@@ -0,0 +1,36 @@
+#! python
+"""
+This Galaxy tool permits to prepare your files to be ready for
+Assembly Hub visualization.
+"""
+
+import sys
+import getopt
+import zipfile
+import subprocess
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print 'hubArchiveCreator.py -i <inputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            # We retrieve the input file
+            inputfile = open(arg, 'r')
+        elif opt in ("-o", "--ofile"):
+            inputfile = open(arg, 'w+')
+            subprocess.Popen(
+                ['Tools/gff3ToGenePred',
+                    inputfile.name,
+                    outputfile.name])
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

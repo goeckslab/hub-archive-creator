9cc29eabfa2dfcf91a6939dd591b7f68d3acafa4
remimarenco <remi.marenco@gmail.com>
2016-01-22 11:34:51 -0500

    Add genePredToBed, sort and bedToBigBed (WIP on this last one)

--- hubArchiveCreator.py
+++ hubArchiveCreator.py
@@ -26,11 +26,47 @@ def main(argv):
             # We retrieve the input file
             inputfile = open(arg, 'r')
         elif opt in ("-o", "--ofile"):
-            inputfile = open(arg, 'w+')
-            subprocess.Popen(
+            outputfile = open(arg, 'w+')
+
+            genePredFile = open('temp.genepred', 'w+')
+            unsortedBedFile = open('temp.unsortedBed', 'w+')
+            sortedBedFile = open('temp.sortedBed', 'w+')
+            bigBedFile = open('temp.bb', 'w+')
+
+            # gff3ToGenePred processing
+            p = subprocess.Popen(
                 ['Tools/gff3ToGenePred',
                     inputfile.name,
-                    outputfile.name])
+                    genePredFile.name])
+            # We need to wait the time gff3ToGenePred terminate so genePredToBed can begin
+            # TODO: Check if we should use communicate instead of wait
+            p.wait()
+
+            # genePredToBed processing
+            p = subprocess.Popen(
+                ['Tools/genePredToBed',
+                    genePredFile.name,
+                    unsortedBedFile.name])
+            p.wait()
+
+            # Sort processing
+            p = subprocess.Popen(
+                ['sort',
+                    '-k'
+                    '1,1',
+                    '-k'
+                    '2,2n',
+                    unsortedBedFile.name,
+                    '-o',
+                    sortedBedFile.name])
+            p.wait()
+
+            # bedToBigBed processing
+            p = subprocess.Popen(
+                ['Tools/bedToBigBed',
+                    sortedBedFile.name,
+                    bigBedFile.name])
+            p.wait()
 
 if __name__ == "__main__":
     main(sys.argv[1:])

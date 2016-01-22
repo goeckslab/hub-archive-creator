454733014c0aa03bcaeb6888ecf411d52c6e076e
remimarenco <remi.marenco@gmail.com>
2016-01-22 18:50:19 -0500

    Removed test code

--- hubArchiveCreator.py
+++ hubArchiveCreator.py
@@ -73,8 +73,5 @@ def main(argv):
             #        bigBedFile.name])
             # p.wait()
 
-            outputfile.write("Coucou")
-            outputfile.seek(0)
-
 if __name__ == "__main__":
     main(sys.argv[1:])

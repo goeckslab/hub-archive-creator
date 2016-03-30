b17e084d404fc02cd1a83c7496f0a0e4d55e28f3
Remi Marenco <remi.marenco@gmail.com>
2016-03-30 09:38:20 -0400

    Add the script to clean everytime we launch locally HAC

---
+++ util/cleanDirectory.py
@@ -0,0 +1,14 @@
+"""Use to clean the directory after the run of HubArchiveCreator.py manually"""
+import os
+import shutil
+
+
+# Remove 'myHub.zip at root folder
+try:
+    os.remove('myHub.zip')
+except OSError as o:
+    # We don't need to crash the program
+    print 'Warning: ' + str(o)
+
+# Remove 'myHub' folder and its content
+shutil.rmtree('myHub', ignore_errors=True)

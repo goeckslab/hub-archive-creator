0ebfd371e6f7e44b4e1998eceb9d6e152426526d
Remi Marenco <remi.marenco@gmail.com>
2016-05-06 15:57:47 -0400

    Add the huba_xml automatically imported

--- util/add_datatype.py
+++ util/add_datatype.py
@@ -9,7 +9,9 @@ Place yourself in the folder of the python script, and launch it
 - Based on the fact datatypes_conf
 """
 
+import shutil
 import sys
+import os
 import xml.etree.ElementTree as ET
 
 def main(argv):
@@ -18,7 +20,7 @@ def main(argv):
     add_hubAssembly()
 
 def add_datatype_conf():
-    print "Add datatype"
+    print "======= Add datatype ======="
     datatype_conf_path = '../../../config/datatypes_conf.xml'
     # TODO: Not relative to this python file but based on a parameter galaxy_root
     # TODO: Check if datatypes_conf.xml, if not create it by copying datatypes_conf.xml.sample
@@ -32,17 +34,21 @@ def add_datatype_conf():
     print registration.attrib
 
     huba_datatype = ET.parse('../hubaDatatype/datatypes_conf.xml').getroot()
+    # TODO: Verify the datatype is not already existing, else do not add / write. And in another version, check it
     registration.append(huba_datatype)
     tree.write(datatype_conf_path)
     print "datatype added in %s" % datatype_conf_path
     return
 
 def add_huba_xml():
-    print "Add hub xml"
+    print "======= Add hub xml ======="
+    displayApp_ucsc_path = "../../../display_applications/ucsc/"
+    shutil.copy("../hubaDataType/huba.xml", displayApp_ucsc_path)
+    print "Content of %s now: %s" % (displayApp_ucsc_path, os.listdir(displayApp_ucsc_path))
     return
 
 def add_hubAssembly():
-    print "Add hubAssembly"
+    print "======= Add hubAssembly ======="
     return
 
 if __name__ == "__main__":

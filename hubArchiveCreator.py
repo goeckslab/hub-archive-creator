0c16e19549f200a1a7e1d293f4d63022262d9877
Remi Marenco <remi.marenco@gmail.com>
2016-06-17 20:13:05 -0400

    Add the display order depending on the order in the tool form

--- hubArchiveCreator.py
+++ hubArchiveCreator.py
@@ -9,6 +9,7 @@ hubArchiveCreator.py -g test-data/augustusDbia3.gff3 -f test-data/dbia3.fa -d . 
 """
 
 import argparse
+import collections
 import json
 import sys
 
@@ -85,6 +86,7 @@ def main(argv):
     json_inputs_data = args.data_json
 
     inputs_data = json.loads(json_inputs_data)
+
     # We remove the spaces in ["name"] of inputs_data
     sanitize_name_inputs(inputs_data)
 
@@ -106,35 +108,43 @@ def main(argv):
     # Create the Track Hub folder
     trackHub = TrackHub(input_fasta_file, outputFile, extra_files_path, toolDirectory)
 
+    all_datatype_dictionary = {}
+
     # Process Augustus
     if array_inputs_gff3:
-        add_track(Gff3, array_inputs_gff3, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(Gff3, array_inputs_gff3, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
 
     # Process Bed simple repeats => From Tandem Repeats Finder / TrfBig
     if array_inputs_bed_simple_repeats:
-        add_track(BedSimpleRepeats, array_inputs_bed_simple_repeats, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(BedSimpleRepeats, array_inputs_bed_simple_repeats, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
 
     # Process a Bed => tBlastN or TopHat
     if array_inputs_bed_generic:
-        add_track(Bed, array_inputs_bed_generic, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(Bed, array_inputs_bed_generic, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
 
     # Process a GTF => Tophat
     if array_inputs_gtf:
-        add_track(Gtf, array_inputs_gtf, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(Gtf, array_inputs_gtf, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
 
     # Process a Bam => Tophat
     if array_inputs_bam:
-        add_track(Bam, array_inputs_bam, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(Bam, array_inputs_bam, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
 
     # Process a BigWig => From Bam
     if array_inputs_bigwig:
-        add_track(BigWig, array_inputs_bigwig, inputs_data, input_fasta_file,
-                  extra_files_path, trackHub, toolDirectory)
+        create_ordered_datatype_objects(BigWig, array_inputs_bigwig, inputs_data, input_fasta_file,
+                                        extra_files_path, all_datatype_dictionary, toolDirectory)
+
+    # Create Ordered Dictionary to add the tracks in the tool form order
+    all_datatype_ordered_dictionary = collections.OrderedDict(all_datatype_dictionary)
+
+    for index, datatypeObject in all_datatype_ordered_dictionary.iteritems():
+        trackHub.addTrack(datatypeObject.track.trackDb)
 
     # We process all the modifications to create the zip file
     trackHub.createZip()
@@ -155,27 +165,29 @@ def sanitize_name_inputs(inputs_data):
         inputs_data[key]["name"] = inputs_data[key]["name"].replace(" ", "_")
 
 
-def add_track(ExtensionClass, array_inputs, inputs_data, input_fasta_file,
-              extra_files_path, trackHub, tool_directory ):
+def create_ordered_datatype_objects(ExtensionClass, array_inputs, inputs_data, input_fasta_file,
+                                    extra_files_path, all_datatype_dictionary, tool_directory):
     """
     Function which executes the creation all the necessary files / folders for a special Datatype, for TrackHub
+    and update the dictionary of datatype
     :param ExtensionClass: T <= Datatype
     :param array_inputs: list[string]
     :param inputs_data:
     :param input_fasta_file: string
     :param extra_files_path: string
-    :param trackHub: TrackHub
     :param tool_directory; string
-    :return:
     """
+
+    datatype_dictionary = {}
+
     # TODO: Optimize this double loop
     for input_false_path in array_inputs:
         for key, data_value in inputs_data.items():
             if key == input_false_path:
                 extensionObject = ExtensionClass(input_false_path, data_value,
                                                  input_fasta_file, extra_files_path, tool_directory)
-                trackHub.addTrack(extensionObject.track.trackDb)
-
+                datatype_dictionary.update({data_value["order_index"]: extensionObject})
+    all_datatype_dictionary.update(datatype_dictionary)
 
 if __name__ == "__main__":
     main(sys.argv)

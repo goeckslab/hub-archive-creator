85389def8ed35b3dc08701963dcae4afd1b5b50a
Remi Marenco <remi.marenco@gmail.com>
2016-07-27 09:42:37 -0500

    Fix BigWig type type

--- BigWig.py
+++ BigWig.py
@@ -29,7 +29,7 @@ class BigWig( Datatype ):
         # Create the Track Object
         self.createTrack(file_path=trackName,
                          track_name=trackName,
-                         long_label=self.name_bigwig, track_type='bigwig', visibility='full',
+                         long_label=self.name_bigwig, track_type='bigWig', visibility='full',
                          priority=self.priority,
                          track_file=myBigWigFilePath)
 
